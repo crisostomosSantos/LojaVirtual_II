3ffa905c8c0e1a3ed33981e998c101d832223ca9
crisostomosSantos <criscgweb@gmail.com>
2015-09-05 19:25:12 -0300

    Listando Produtos

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidade;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+   public  class Produto
+    {
+        public int ProdutoID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+         
+    }
+}
