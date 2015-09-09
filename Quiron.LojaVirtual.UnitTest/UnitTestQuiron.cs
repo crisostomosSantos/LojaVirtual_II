4aa36c666129a51e9014d131e29c6f457c67197b
crisostomosSantos <criscgweb@gmail.com>
2015-09-09 09:44:04 -0300

    aula 12

--- Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.Controllers.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 
 namespace Quiron.LojaVirtual.UnitTest
@@ -20,7 +22,7 @@ namespace Quiron.LojaVirtual.UnitTest
             CollectionAssert.AreEqual(resultado.ToArray(), teste);
 
         }
-
+        [TestMethod]
         public void Skip()
         {
 
@@ -31,17 +33,38 @@ namespace Quiron.LojaVirtual.UnitTest
             CollectionAssert.AreEqual(resultado.ToArray(), teste);
 
         }
+        [TestMethod]
+        public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
+        {
+            //Arrange
+
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            //ACT
+
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
 
 
-    
-    
-    
-    
+
+                //html.PageLinks(Paginacao, paginaUrl);
+
+           //Assert
+
+            Assert.AreEqual(
+                @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+              + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+              + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                );
+        }
     }
-
-
-
-
-
 }
 
--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -1,4 +1,5 @@
 ﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,19 +11,32 @@ namespace Quiron.LojaVirtual.Web.Controllers
     public class VitrineController : Controller
     {
         private ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 8; 
 
-        public int ProdutosPorPagina = 5; 
-        // GET: Vitrine
-        public ActionResult ListaProdutos( int pagina = 1)
+        public ViewResult ListaProdutos( int pagina = 1)
         {
-
              _repositorio = new ProdutosRepositorio();
-             var produtos = _repositorio.Produto
-                 .OrderBy(p => p.Descricao)
-            .Skip((pagina - 1) * ProdutosPorPagina)
-            .Take(ProdutosPorPagina);
+
+             ProdutosViewModel model = new ProdutosViewModel
+             {
+
+                 Produtos = _repositorio.Produto
+                     .OrderBy(p => p.Descricao)
+                     .Skip((pagina - 1) * ProdutosPorPagina)
+                     .Take(ProdutosPorPagina),
+
+                 Paginacao = new Paginacao
+                 {
+                     PaginaAtual = pagina,
+                     ItensPorPagina = ProdutosPorPagina,
+                     ItensTotal = _repositorio.Produto.Count()
+
+                 }
+             };
+
+            
        
-        return View(produtos);
+        return View(model);
         }
     }
 }
